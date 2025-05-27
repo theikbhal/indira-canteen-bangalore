80d534f51756fd29d7f7a7b92cf671f7432ce784
theikbhal <theikbhal@gmail.com>
2025-05-27 14:55:57 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const canteens = [
+  {
+    name: "Indira Canteen - Koramangala",
+    lat: 12.9352,
+    lng: 77.6143,
+    locality: "Koramangala"
+  },
+  {
+    name: "Indira Canteen - Whitefield",
+    lat: 12.9698,
+    lng: 77.7499,
+    locality: "Whitefield"
+  },
+  {
+    name: "Indira Canteen - Jayanagar",
+    lat: 12.9250,
+    lng: 77.5938,
+    locality: "Jayanagar"
+  }
+];
